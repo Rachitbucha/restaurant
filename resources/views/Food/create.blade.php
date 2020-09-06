c69e312e9191951d4d7f8c93f962467604449608
Rachit <Rachit.rj11@gmail.com>
2020-09-06 20:22:09 +0530

    validation of the form

--- resources/views/Food/create.blade.php
+++ resources/views/Food/create.blade.php
@@ -4,29 +4,63 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+
+        <form action="{{route('food.store')}}" method="post">@csrf
             <div class="card">
                 <div class="card-header">Add new Food</div>
 
                 <div class="card-body">
                   <div class="form-group">
                     <label for="name">Name</label>
-                    <input type="text" name="name" class="form-control">
+                    <input type="text" name="name" class="form-control @error('name') is-invalid @enderror">
+                    @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                    @enderror
                   </div>
                   <div class="form-group">
                     <label for="description">Description</label>
-                    <input type="text" name="description" class="form-control">
+                    <textarea name="description" class="form-control @error('description') is-invalid @enderror" cols="30" rows="10"></textarea>
+                    @error('description')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                    @enderror
                   </div>
                   <div class="form-group">
                     <label for="price">Price</label>
-                    <input type="number" name="price" class="form-control">
+                    <input type="number" name="price" class="form-control @error('price') is-invalid @enderror">
+                    @error('price')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                    @enderror
                   </div>
                   <div class="form-group">
                     <label for="name">Category</label>
-                    <input type="text" name="name" class="form-control">
+                    <select name="category" class="form-control @error('category') is-invalid @enderror" id="">
+                        @foreach(App\Category::all() as $category)
+                            <option value="">Select Category</option>
+                            <option value="{{$category->id}}">
+                                {{$category->name}}
+                            </option>
+                        @endforeach
+                    </select>
+                    @error('category')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                    @enderror
                   </div>
                   <div class="form-group">
                     <label for="image">Image</label>
-                    <input type="text" name="image" class="form-control">
+                    <input type="text" name="image" class="form-control @error('image') is-invalid @enderror">
+                    @error('image')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                    @enderror
                   </div> 
 
                   <div class="form-group">
@@ -35,6 +69,7 @@
 
                 </div>
             </div>
+            </form>
         </div>
     </div>
 </div>
