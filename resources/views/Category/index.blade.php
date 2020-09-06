c9cae6f60a65677c7a6d9e2f41d6ecd5d9625f79
Rachit <Rachit.rj11@gmail.com>
2020-09-06 14:01:18 +0530

    edit category

--- resources/views/Category/index.blade.php
+++ resources/views/Category/index.blade.php
@@ -8,9 +8,35 @@
                 <div class="card-header">All Category</div>
 
                 <div class="card-body">
-                    @foreach($categories as $category)
-                        <p>{{$category->name}}</p>
-                    @endforeach            
+                    <table class="table">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Edit</th>
+                                <th scope="col">Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        @if(count($categories)>0)
+                        @foreach($categories as $key=>$category)
+                            <tr>
+                                <th scope="row">{{$key+1}}</th>
+                                <td>{{$category -> name}}</td>
+                                <td>
+                                    <a href="{{route('category.edit', [$category->id])}}"><button class="btn btn-outline-success">Edit</button></a>
+                                </td>
+                                <td>
+                                <a href="{{route('category.destroy', [$category->id])}}"><button class="btn btn-outline-danger">Delete</button></a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        @else
+                            <td> No Category to Display </td>
+                        @endif
+                        </tbody>
+
+                    </table>
                 </div>
             </div>
         </div>
